b9d8795d27bfe58ba8a1ace8167ffacd41661968
Henrique <henrique@xonha.space>
2024-10-05 14:58:58 -0300

    feat: middleware protect admin routes

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
+import { createUser, readUserWithRole } from "./app/(authPages)/users/actions";
 import { updateServerSession } from "./supabase/middleware";
 import { useSupabaseServer } from "./supabase/server";
-import { createUser } from "./app/(authPages)/users/actions";
 
 const publicPathnames = ["/login", "/password"];
+const adminPathnames = ["/users", "/cashflow", "/periods"];
 
 export async function middleware(req: NextRequest) {
   const server = await useSupabaseServer();
   const origin = `${req.headers.get("x-forwarded-proto")}://${req.headers.get("host")}`;
+  const authUser = await server.auth.getUser();
+  const userRole = await readUserWithRole();
   const reqUrl = new URL(req.url);
   const res = NextResponse.next();
-  res.headers.set("origin", origin);
 
+  res.headers.set("origin", origin);
   updateServerSession(req);
 
-  const authUser = await server.auth.getUser();
-
-  if (authUser.error && !publicPathnames.includes(req.nextUrl.pathname)) {
+  if (
+    authUser.error ||
+    (userRole.userRole !== "admin" &&
+      adminPathnames.includes(req.nextUrl.pathname)) ||
+    (authUser.error && !publicPathnames.includes(req.nextUrl.pathname))
+  ) {
     return NextResponse.redirect(`${reqUrl.origin}`);
-  }
-
-  if (authUser.error) {
-    return NextResponse.redirect(
-      `${reqUrl.origin}/login?error=${authUser.error.message}`,
-    );
   }
 
   const foundUser = await server
