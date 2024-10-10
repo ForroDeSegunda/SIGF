b01d304d1e21c3321b4d91de16ae4fcac744c598
Henrique <henrique@xonha.space>
2024-10-09 22:04:42 -0300

    feat: on import enrollments create users concurrently

--- app/(authPages)/classes/[id]/actions.ts
+++ app/(authPages)/classes/[id]/actions.ts
@@ -42,42 +42,60 @@ export async function createUsersForXlsxImport(
   const supabaseServer = await useSupabaseServer();
   const session = await supabaseServer.auth.getSession();
 
-  let createdUsers: TUserViewPlusRole[] = [];
+  // Helper function to handle user creation and updates concurrently
+  async function createUser(
+    user: TUserCreate,
+  ): Promise<TUserViewPlusRole | null> {
+    try {
+      const { data, error } = await supabaseServer.auth.signUp({
+        email: user.email,
+        password: createHash("sha256").update("password").digest("hex"),
+      });
 
-  for (const user of users) {
-    const { data, error } = await supabaseServer.auth.signUp({
-      email: user.email,
-      password: createHash("sha256").update("password").digest("hex"),
-    });
-    if (error) {
-      console.error("Error creating user:", error);
-      continue;
-    }
+      if (error) {
+        console.error("Error creating user:", error);
+        return null;
+      }
 
-    await supabaseServer.auth.updateUser({
-      data: {
-        full_name: user.full_name,
-      },
-    });
+      await supabaseServer.auth.updateUser({
+        data: {
+          full_name: user.full_name,
+        },
+      });
 
-    const newUser: TUserViewPlusRole = {
-      id: data.user?.id!,
-      email: data.user?.email!,
-      full_name: user.full_name,
-      user: {
+      const newUser: TUserViewPlusRole = {
         id: data.user?.id!,
-        role: "student",
-      },
-    };
-    createdUsers.push(newUser);
+        email: data.user?.email!,
+        full_name: user.full_name,
+        user: {
+          id: data.user?.id!,
+          role: "student",
+        },
+      };
 
-    supabaseServer.auth.resetPasswordForEmail(data.user?.email!);
+      // Send reset password email
+      await supabaseServer.auth.resetPasswordForEmail(data.user?.email!);
+
+      return newUser;
+    } catch (err) {
+      console.error("Unexpected error:", err);
+      return null;
+    }
   }
 
+  // Use Promise.all to create users concurrently
+  const createdUsers = await Promise.all(users.map(createUser));
+
+  // Filter out any null results (for cases where user creation failed)
+  const validCreatedUsers = createdUsers.filter(
+    (user) => user !== null,
+  ) as TUserViewPlusRole[];
+
+  // Restore the session
   await supabaseServer.auth.setSession({
     refresh_token: session.data.session?.refresh_token!,
     access_token: session.data.session?.access_token!,
   });
 
-  return createdUsers;
+  return validCreatedUsers;
 }
